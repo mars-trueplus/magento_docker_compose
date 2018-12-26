520d1bf17bd338bf546a05c888c37dd9c0a826e7
XMars <mars@trueplus.vn>
2018-12-26 09:33:19 +0700

    [ADD] magento2* dockefile

---
+++ README.md
@@ -0,0 +1,4 @@
+# Instructions
+
++ Update `BUILD_VERSION` variable in file `generate_apache_dockerfile.py` to point out what are apache_php* and magento* images version
++ Update `REPO_OWNER` to your username on docker hub
--- generate_apache_dockerfile.py
+++ generate_apache_dockerfile.py
@@ -1,9 +1,98 @@
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup
+from operator import itemgetter
+from fabric import Connection
 import os
 import re
-from fabric import Connection
+import requests
 import subprocess
+
+BUILD_VERSION = '1'
+REPO_OWNER = 'marstrueplus'
+
+
+def value_contain_string(val, c_str):
+    return c_str in val
+
+
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except ValueError as e:
+        return False
+
+
+def get_stable_releases():
+    res = []
+    url = 'http://php.net/downloads.php'
+    data = requests.get(url).text
+    soup = BeautifulSoup(data, 'html.parser')
+    h3_tag_version = soup.find_all('h3', class_='title')
+    php_versions = []
+
+    for tag in h3_tag_version:
+        php_versions.append(re.sub('[^0-9.]', '', tag['id']))
+
+    php_versions = [v for v in php_versions if int(v.split('.')[0]) >= 7]
+
+    for php_version in php_versions:
+        sha256 = soup.find('a', string='php-%s.tar.xz' % php_version).find_next_sibling('span', class_='sha256').text
+        res.append({
+            'php_version': php_version,
+            'sha256': sha256
+        })
+
+    return res
+
+
+def get_unsupported_releases():
+    res = []
+    url = 'http://php.net/releases/'
+    data = requests.get(url).text
+    soup = BeautifulSoup(data, 'html.parser')
+
+    php_versions = []
+    h2_tags = soup.find_all('h2')
+    for tag in h2_tags:
+        php_version = tag.text
+        part_versions = [int(x) for x in php_version.split('.') if is_int(x)]
+        if part_versions[1] >= 1:
+            a = 1
+        if len(part_versions) < 3 or \
+                part_versions[0] < 7 or \
+                (part_versions[0] == 7 and part_versions[1] == 0 and part_versions[2] < 13):
+            continue
+        php_versions.append(php_version)
+    for php_version in php_versions:
+        sha256 = soup.find(href=re.compile('%s.tar.xz' % php_version)).find_next_sibling('span', class_='sha256sum').text.split(' ')[-1]
+        res.append({
+            'php_version': php_version,
+            'sha256': sha256
+        })
+
+    return res
+
+
+def get_all_php_releases():
+    # get only php version >= 7.0.13 (magento 2.2.x - 2.3.x)
+    stable_releases = get_stable_releases()
+    unsupported_releases = get_unsupported_releases()
+    all = stable_releases + unsupported_releases
+    res = [info for info in all]
+    res = sorted(res, key=itemgetter('php_version'), reverse=True)
+
+    with open('php_info/php_releases.csv', 'r') as f:
+        existed_lines = f.read()
+
+    with open('php_info/php_releases.csv', 'a+') as f:
+        for php_info in res:
+            line = '%s %s' % (php_info['php_version'], php_info['sha256'])
+            if line not in existed_lines:
+                f.write(line + '\n')
+
+    return True
 
 
 def get_gpg_keys(php_version):
@@ -20,20 +109,28 @@ def get_gpg_keys(php_version):
 
 
 def get_php_versions():
-    php_version_f = 'php_info/php_versions.csv'
+    php_version_f = 'php_info/php_releases.csv'
     with open(php_version_f, 'r') as f:
-        php_versions = f.readlines()
+        php_versions = [line.split(' ')[0] for line in f.readlines()]
 
     return php_versions
+
+
+def get_php_releases():
+    php_releases_file = 'php_info/php_releases.csv'
+    with open(php_releases_file, 'r') as f:
+        php_releases = f.readlines()
+
+    return php_releases
 
 
 def generate_apache_php_build_files():
     # generate dockerfiles: all docker php version
     demo_file = open('all/demo', "r+")
     demo_content = demo_file.readlines()
-    php_versions = get_php_versions()
+    php_releases = get_php_releases()
 
-    for line in php_versions:
+    for line in php_releases:
         new_content = demo_content.copy()
         PHP_VERSION = line.split(' ')[0]
         PHP_SHA256 = line.split(' ')[1].replace('\n', '')
@@ -73,9 +170,8 @@ def generate_apache_php_build_folder():
     local_con = Connection('localhost')
     php_versions = get_php_versions()
 
-    for line in php_versions:
-        PHP_VERSION = line.split(' ')[0]
-        docker_filename = 'apache2-php%s' % PHP_VERSION
+    for php_version in php_versions:
+        docker_filename = 'apache2-php%s' % php_version
         dic_src = "build_folder/demo"
         dic_dest = "build_folder/%s" % docker_filename
         dic_src_path = os.path.abspath(dic_src)
@@ -89,8 +185,25 @@ def generate_apache_php_build_folder():
         file_dest_path = os.path.abspath(file_dest)
         local_con.local('cp -r %s %s' % (file_src_path, file_dest_path))
 
-    all_apache_files = os.path.abspath('all')
-    # local_con.local('rm -rf %s/apache*' % all_apache_files)
+
+def build_apache_php_images():
+    php_versions = get_php_versions()
+    for php_version in php_versions:
+        docker_filename = "apache2-php%s" % php_version
+        dockerfile = "build_folder/{0}/{0}".format(docker_filename)
+        dockerfile_path = os.path.abspath(dockerfile)
+        build_context = "build_folder/%s" % (docker_filename)
+        build_context_path = os.path.abspath(build_context)
+        build_command = "docker build -f {dockerfile_path} -t {repo_owner}/{image_name}:{image_version} {build_context_path}".format(
+            dockerfile_path=dockerfile_path,
+            repo_owner=REPO_OWNER,
+            image_name=docker_filename,
+            image_version=BUILD_VERSION,
+            build_context_path=build_context_path
+        )
+        start_str = "*" * 50 + "\n" + "*" * 50 + "\nStart building %s image \n" % docker_filename + "*" * 50 + "\n" + "*" * 50 + "\n"
+        subprocess.call(["echo", start_str])
+        subprocess.call(build_command, shell=True)
 
 
 def generate_magento_build_files():
@@ -110,25 +223,10 @@ def generate_magento_build_files():
             f.writelines(new_content)
 
 
-def build_apache_php_images():
-    php_versions = get_php_versions()
-    for line in php_versions:
-        php_version = line.split(' ')[0]
-        docker_filename = "apache2-php%s" % php_version
-        dockerfile = "build_folder/{0}/{0}".format(docker_filename)
-        dockerfile_path = os.path.abspath(dockerfile)
-        build_context = "build_folder/%s" % (docker_filename)
-        build_context_path = os.path.abspath(build_context)
-        build_command = "docker build -f %s -t marstrueplus/%s %s" % (dockerfile_path, docker_filename, build_context_path)
-        start_str = "*" * 50 + "\n" + "*" * 50 + "\nStart building %s image \n" % docker_filename + "*" * 50 + "\n" + "*" * 50 + "\n"
-        subprocess.call(["echo", start_str])
-        subprocess.call(build_command, shell=True)
-
-
 if __name__ == '__main__':
     # run separate below functions
+    # get_all_php_releases()
     # generate_apache_php_build_files()
     # generate_apache_php_build_folder()
-    build_apache_php_images()
-    # generate_magento_build_files()
-    pass
+    # build_apache_php_images()
+    generate_magento_build_files()
